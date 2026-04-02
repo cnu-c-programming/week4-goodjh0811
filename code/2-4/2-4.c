0a151125b1061dc39cb50ebba1f39aad5734c7e1
박지현202500924 <goodjh0811@gmail.com>
2026-04-02 23:20:33 +0900

    Update 2-4.c

--- code/2-4/2-4.c
+++ code/2-4/2-4.c
@@ -3,10 +3,10 @@
 #include <ctype.h>
 
 int main(){
-    char chars[] = {'A','z','5','!',' '};
+    char chars[] = {'A', 'z', '5', '!', ' '};
     int n = sizeof(chars)/sizeof(chars[0]);
 
-    for(int i=0;i<n;i++){
+    for(int i=0; i<n; i++){
         char ch = chars[i];
         printf("%c - alpha:%d digit:%d upper:%c lower:%c\n",
         ch, 
