010704386e0a9bf7c8dfa7f8470bbfe6994c71b3
박지현202500924 <goodjh0811@gmail.com>
2026-04-02 10:23:27 +0900

    Update 2-3.c

---
+++ code/2-3/2-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include 
+
+int main() {
+  char a[64] = "Hello";
+  char b[64] = "world";
+  char c[128];
+
+  printf("length of a: %d\n", len);
+  printf("a: %s, c: %s\n", a, c);
+  printf("c: %s\n", c);
+  printf("strcmp(%s, %s) + %d\n", a, a,);
+  printf("strcmp(%s, %s) + %d\n", a, b,);
+
+  return 0;
+}
+
+
+
+
