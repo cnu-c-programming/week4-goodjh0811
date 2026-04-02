64638dfd70945da81c2fb1b1228ca5e825bd9403
박지현202500924 <goodjh0811@gmail.com>
2026-04-02 22:09:37 +0900

    Update 2-4.c

--- code/2-4/2-4.c
+++ code/2-4/2-4.c
@@ -3,11 +3,16 @@
 
 int main() {
   char chars[] = {'A', 'z', '5', '!', ' '};
-int len sizeof(chars) / sizeof(chars[0]);
+  int len = sizeof(chars) / sizeof(chars[0]);
 
 for (int i = 0; i< len; i++) {
 char ch = chars[i];
-printf("%c - alpha: %d digit: %d upper: %c lower: %c\n", ch, isalpha(ch), isdigit(ch), isupper(ch), islower(ch));
+printf("%c - alpha: %d digit: %d upper: %d lower: %c\n", 
+  ch, 
+  isalpha(ch), 
+  isdigit(ch), 
+  isupper(ch), 
+  islower(ch));
 }
 return 0;
 }
