157815e6903aa94bb62206c5501b2dc51a79c7d7
박지현202500924 <goodjh0811@gmail.com>
2026-04-02 22:00:49 +0900

    Update 2-1.c

--- code/2-1/2-1.c
+++ code/2-1/2-1.c
@@ -6,7 +6,7 @@ char buffer[64];
 int year, month, day;
 
 sscanf(date, "%d-%d-%d", &year, &month, &day);
-sprintf(buffer, "%d-%d-%d", year, month, day);
+sprintf(buffer, "%d/%d/%d",day, month, year);
 
 printf("%s\n", buffer);
 return 0;
