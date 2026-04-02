bf010e15c885d099f64b9f5a4e220dbf3b26bcf6
박지현202500924 <goodjh0811@gmail.com>
2026-04-02 22:20:22 +0900

    Update 3-2.c

--- code/3-2/3-2.c
+++ code/3-2/3-2.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 void swap_endian(int * x){
+  unsigned char *p = (unsigned char*)x;
+  unsigned char temp:
+
+    temp = p[0];
+    p[0] = p[3];
+    p[3] = temp;
+
+    temp = p[1];
+    p[1] = p[2];
+    p[2] = temp;
 }
 
 int main() {
   int x = 0x12345678;
-  printf("%x\n", x);
+  printf("Before swap: %x\n", x);
   swap_endian(&x);
-  printf("%x\n", x);
+  printf("After swap: %x\n", x);
 
   return 0;
 }
