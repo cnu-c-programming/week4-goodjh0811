858c8631f72c2b4ae1c8557370da4fad3056cbb9
박지현202500924 <goodjh0811@gmail.com>
2026-04-02 22:04:27 +0900

    Update 2-3.c

--- code/2-3/2-3.c
+++ code/2-3/2-3.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
-#include 
+#include <string.h>
 
 int main() {
   char a[64] = "Hello";
   char b[64] = "world";
   char c[128];
 
+int len = strlen(a);
+  printf("문자열 길이\n");
   printf("length of a: %d\n", len);
+
+  strcpy(c, a)
   printf("a: %s, c: %s\n", a, c);
   printf("c: %s\n", c);
-  printf("strcmp(%s, %s) + %d\n", a, a,);
-  printf("strcmp(%s, %s) + %d\n", a, b,);
+
+  printf("문자열 합\n")
+  printf("strcmp(%s, %s) + %d\n", a, a, strcmp(a, a));
+  printf("strcmp(%s, %s) + %d\n", a, b, strcmp(a, b));
 
   return 0;
 }
