25af0e1a60c328a50e21f74466bad0394fb587ff
박지현202500924 <goodjh0811@gmail.com>
2026-04-02 12:09:11 +0900

    Update 2-5.c

---
+++ code/2-5/2-5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include
+
+int main() {
+  double x = 9.0;
+  double y = 2.5;
+
+  printf("%.2f\n", (x));
+  printf("%.2f\n", (x,y));
+  printf("%.1f\n", (y));
+  printf("%.1f\n", (y));
+
+
+  return 0;
+}
+
+
